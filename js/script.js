fa2db604d1aaf919e5ff437b698e59399a8010e2
Autumn <autumncbullard@gmail.com>
2018-11-27 16:42:04 -0500

    Create SVG

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function displayGraph() {
 
   axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json').then((dataset) => {
+    const w = 700;
+    const h = 500;
+    const svg = d3.select('.graph')
+      .append('svg')
+      .attr('width', w)
+      .attr('height', h);
   }).catch(() => {
     document.querySelector('.error-message').style.display = 'block';
   });
