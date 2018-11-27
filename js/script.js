ed4d2ef0e6bf6102c80bfb3a958c0024dcabc491
Autumn <autumncbullard@gmail.com>
2018-11-27 14:49:48 -0500

    Make GET request to API using axios

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function displayGraph() {
+
+  axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json').then((dataset) => {  
+  }).catch(() => {
+  });
+}
+
+displayGraph();
